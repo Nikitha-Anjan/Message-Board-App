be6ad94deb5204d9c8eef8fba90a610b0970fcdd
Anjan MK <nikitha.dc@gmail.com>
2020-09-13 21:17:11 -0400

    second change

--- src/index.js
+++ src/index.js
@@ -4,11 +4,8 @@ import App from './App';
 // npm install react-redux
 // the provider component will make the store available to all the components
 import { Provider } from 'react-redux'
-
 import configureStore from './store/configureStore'
-// import { increment } from './actions/countAction'
-import {startGetUsers} from './actions/usersAction'
-import {startGetPosts} from './actions/postsAction'
+
 
 
 const store = configureStore()
@@ -17,16 +14,12 @@ console.log('redux initial state', store.getState())
 store.subscribe(() => {
   console.log('store updated', store.getState())
 })
-// store.dispatch(increment())
+
 const jsx = (
   <Provider store={store}>
     <App />
   </Provider>
 )
-
-//handle page reloads
-store.dispatch(startGetUsers())
-store.dispatch(startGetPosts())
 
 
 ReactDOM.render(jsx,
